2fe950c2e691c6d8ebdbcef4b009992c64f5f556
xws117 <1179066213@qq.com>
2017-08-21 18:12:26 +0800

    run bug

--- zqy.cpp
+++ zqy.cpp
@@ -5,6 +5,7 @@
 # include <time.h>
 # include <string.h>
 # include <algorithm>
+
 using namespace std;
 
 int num;//工地的数量
@@ -16,11 +17,17 @@ int car_total =0; //所需要的car的总的数量
 int car_cnt = 5;  //车的数量
 int car_get_time[100]; //车辆到达搅拌站的路程的时间
 int chuning[100];  //限制条件  每个工厂的初凝时间
-int dnas[1000][1000]; // 所有的dna
+
+struct DNAS{
+    int dna[1000];
+    int val;
+}dnas[1000]; // 所有的dna
+int chengfa[100]; //惩罚因子
 struct car_arrive{
     int id ;
     int time;
 };
+car_arrive arrive[1000];
 struct car{
     int id;
     int val;
@@ -29,6 +36,11 @@ int cmp ( const void *p1 , const void *p2 ){
     struct car *c=(car*)p1;
     struct car *c1=(car*)p2;
     return c->val-c1->val;
+}
+int dna_cmp ( const void *p1 , const void *p2 ){
+    struct DNAS *c=(DNAS*)p1;
+    struct DNAS *c1=(DNAS*)p2;
+    return c->val<c1->val;
 }
 int get_min(int * car_time,int car_cnt){
     car E[car_cnt];
@@ -39,12 +51,6 @@ int get_min(int * car_time,int car_cnt){
     qsort(E,car_cnt,sizeof(E[0]),cmp);
     return E[0].id;
 }
-double value(int * dna,int cnt){
-    for(int i=0;i<cnt;i++){
-        printf("aqwer %d",dna[i]);
-    }
-    return 0;
-}
 int chuning_check(int *dna,int cnt,int car_cnt){  // 初凝检测
     int mark =0;//0 代表成功 1 代表失败
     //int timestamp[1000];
@@ -53,7 +59,6 @@ int chuning_check(int *dna,int cnt,int car_cnt){  // 初凝检测
     for(int i=0;i<car_cnt;i++){
         car_time_cnt[i]=0;
     }
-    car_arrive arrive[1000];
     memset(arrive,0,sizeof(1000));
     for(int i=0;i<cnt;i++){
         arrive[i].id = dna[i];
@@ -62,7 +67,9 @@ int chuning_check(int *dna,int cnt,int car_cnt){  // 初凝检测
     for(int i=0;i<cnt;i++){
         int tmp = get_min(car_time_cnt,car_cnt);
         arrive[i].id = dna[i];
+        //printf("dna[i] == %d ",dna[i]);
         arrive[i].time = car_get_time[dna[i]];
+        //printf("arrive time =  %d ",arrive[i].time);
         car_time_cnt[tmp]+=car_get_time[dna[i]]*2;
 
     }
@@ -84,12 +91,17 @@ int chuning_check(int *dna,int cnt,int car_cnt){  // 初凝检测
         }
 
     }
+    /*printf("\n");
+    for(int i=0;i<cnt;i++){
+        printf("%d ",arrive[i].time);
+    }
+    printf("\n");*/
     return mark;
 }
 void to_swap(int i,int one ,int two){
-    int tmp = dnas[i][one];
-    dnas[i][one] = dnas[i][two];
-    dnas[i][two] = tmp;
+    int tmp = dnas[i].dna[one];
+    dnas[i].dna[one] = dnas[i].dna[two];
+    dnas[i].dna[two] = tmp;
 
 
 }
@@ -97,19 +109,45 @@ void exchange(int cnt){
     srand((unsigned)time(NULL));
     for(int i=1;i<1000;i++){
         for(int k=0;k<cnt;k++){
-            dnas[i][k] = dnas[i-1][k];
+            dnas[i].dna[k] = dnas[i-1].dna[k];
         }
-        for(int k=0;k<5;k++){
+        for(int k=0;k<3;k++){
             int tmp_one = rand()%cnt;
             int tmp_two = rand()%cnt;
             to_swap(i,tmp_one,tmp_two);
         }
     }
 }
+void check_fales_deal(int  a){
+
+}
+double value(int car,int cnt){
+    double ans =0;
+    //printf("car_cnt==%d\n",car_cnt);
+    for(int i=0;i<car_cnt;i++){
+        int mark = -1;
+        for(int k=0;k<cnt;k++){
+            if(mark==-1&&dnas[car].dna[k]==i){
+                mark = arrive[k].time;
+            }else if(mark!=-1&&dnas[car].dna[k]==i){
+                ans += (arrive[k].time-mark)*chengfa[i];
+                mark = arrive[k].time;
+            }
+        }
+    }
+    return ans;
+}
 int main(){
     scanf("%d",&num);
+    //设置每辆车的惩罚因子
+    for(int i=0;i<car_cnt;i++){
+        chengfa[i] = i*10;
+    }
     int dna[1000];
     //begin 假数据
+    for(int i=0;i<num;i++){
+        chengfa[i] = i;
+    }
     for(int i=0;i<num;i++){
         car_get_time[i] = 10+10*i;
     }
@@ -136,7 +174,14 @@ int main(){
     for(int i=0;i<cnt;i++){
         printf("%d ",dna[i]);
     }
-    double ans = value(dna,cnt);
+
+
+    //把dnas初始化
+    for(int i=0;i<1000;i++){
+        dnas[i].val = 99999999;
+    }
+
+    //double ans = value(dna,cnt);
     //time_t timep;
 
     //time(&timep); /*获取time_t类型的当前时间*/
@@ -146,20 +191,24 @@ int main(){
     //printf("%s", asctime(localtime(&timep)));
     //printf("%d",timep);
     //从数据库读取一个时间戳
-    for(int i=0;i<)
-    for(int i=0;i<500;i++){
-        for(int j=0;j<10;j++){
-            exchange(cnt);  //染色体交换
-            for(int i=0;i<100;i++){
-                int mark = chuning_check(dnas[i],cnt,car_cnt);  //完成染色体交换以后对所有的染色体进行初凝检测
-                if(mark==1){
-                   check_fales_deal(i)//对失败的进行标记
-                }else{   //对所有的合格的颜色体进行value计算
-                    int val = value(i);
-                    //找出最大值进行标记
-                    //对所有的失败的染色体进行重新dna的编写，把最优解或者最优解的变种赋值给他
-                }
+    for(int i=0;i<5;i++){
+        printf("所用到的车次为 ===  %d\n",cnt);
+        exchange(cnt);  //染色体交换
+        for(int i=0;i<100;i++){
+            int mark = chuning_check(dnas[i].dna,cnt,car_cnt);  //完成染色体交换以后对所有的染色体进行初凝检测
+            if(mark==1){
+               check_fales_deal(i);//对失败的进行标记
+            }else{   //对所有的合格的颜色体进行value计算
+                int val = value(i,cnt);
+                dnas[i].val = val;
+                printf("%d ",val);
+                //找出最大值进行标记
+                //对所有的失败的染色体进行重新dna的编写，把最优解或者最优解的变种赋值给他
             }
+        }
+        qsort(dnas,1000,sizeof(dnas[0]),dna_cmp);
+        for(int i=0;i<cnt;i++){
+            printf("%d ",dnas[0].dna[i]);
         }
     }
     return 0;
